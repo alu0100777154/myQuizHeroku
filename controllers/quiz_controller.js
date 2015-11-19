8ebb85611a3020eb8d79ded667c8e647f36443ff
Guillermo Sánchez <alu0100777154@ull.edu.es>
2015-11-19 11:48:20 +0000

    first

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,34 @@
+var Quiz = require('../models/quiz_model.js');
+
+var quiz = new Quiz();
+var current = quiz.randomQuestion();
+
+exports.index = function(req, res, next) {
+  res.render('index', { title: 'Quiz' });
+};
+
+exports.question = function(req,res) {
+  current = quiz.randomQuestion();
+  res.render('quizes/question', {pregunta: current.pregunta});
+};
+
+exports.answer = function(req, res) {
+  var c = 'Incorrecto';
+  if (current.respuesta(req.query.respuesta)) { c = 'Correcto'; }
+  res.render('quizes/answer', {respuesta: c});
+};
+
+exports.pregunta = function(req, res) {
+  var id = req.params.id;
+  var n = quiz.numQ();
+
+  if(id < 1 || id > n){
+    res.render('quizes/question', {prg: "No existe esa pregunta."})
+  }
+
+  else {
+   current = quiz.q[id-1];
+
+   res.render('quizes/question', {pregunta: current.pregunta});
+ }
+};
