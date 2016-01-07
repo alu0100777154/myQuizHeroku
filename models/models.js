108caef04382889f87a196ddfb8fdc7c07bf3739
Guille <alu0100777154@ull.edu.es>
2016-01-07 18:02:58 +0000

    sqlite?

--- models/models.js
+++ models/models.js
@@ -2,7 +2,9 @@ var path = require('path');
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite   DATABASE_URL = sqlite://:@:/
 
-var databaseURL = process.env.DATABASE_URL || 'postgres://user:passwd@host:port/database';
+//var databaseURL = process.env.DATABASE_URL || 'postgres://user:passwd@host:port/database';
+
+var databaseURL = process.env.DATABASE_URL          || 'sqlite://:@:/';
 
 
 //var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
