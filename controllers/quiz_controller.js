897ebb0d79620aa8f87e6af5ab069780476cb35d
alu0100777154 <alu0100777154@ull.edu.es>
2016-01-07 19:08:17 +0000

    ById

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,14 +2,14 @@ var models = require('../models/models.js');
 
 //GET /quizes/question
 exports.show = function (req, res) {
-    models.Quiz.find(req.params.quizId).then(function (quiz) {
+    models.Quiz.findById(req.params.quizId).then(function (quiz) {
         res.render('quizes/show', {quiz: quiz});
     });
 };
 
 
 exports.answer = function (req, res) {
-    models.Quiz.find(req.params.quizId).then(function (quiz) {
+    models.Quiz.findById(req.params.quizId).then(function (quiz) {
         if (req.query.respuesta === quiz.respuesta) {
             res.render('quizes/answer',
                     {quiz: quiz, respuesta: 'Correcto'});
