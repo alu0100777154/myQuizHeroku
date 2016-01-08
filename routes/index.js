aecfad9db0791c68515279db58d3fbfc26c9ae70
alu0100777154 <alu0100777154@ull.edu.es>
2016-01-08 04:04:48 +0000

    moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,8 @@ router.get('/', function (req, res, next) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
+
 
 // Definicion de rutas de sesion
 router.get('/login', sessionController.new);
@@ -34,6 +36,8 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 //Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+        sessionController.loginRequired, commentController.publish);
 
 
 module.exports = router;
