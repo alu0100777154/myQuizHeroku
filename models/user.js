45280b00fc89613680946b52bd8c7b3942c0d662
alu0100777154 <alu0100777154@ull.edu.es>
2016-01-08 04:19:30 +0000

    modelo user

---
+++ models/user.js
@@ -0,0 +1,14 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      validate: { notEmpty: {msg: "-> Falta username." }}
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      validate: { notEmpty: {msg: "-> Falta password."}}
+    },
+  });
+}
